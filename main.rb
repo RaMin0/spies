f7a8553549b132ca021b66eef8adb1a03b384906
Ramy Aboul Naga <ramy.naga@gmail.com>
2019-08-06 13:24:33 +0200

    Add seats left

--- main.rb
+++ main.rb
@@ -14,13 +14,14 @@ URL = 'https://www.spies.dk/afbudsrejser?filter=false&search=false' +
 doc = Nokogiri::HTML(open(URL))
 puts ' +-----------------+'
 puts ' | Trips to: Egypt |'
-puts '+------------+-----+-----------+-------------+'
-puts '| Date       | Duration (days) | Price (kr.) |'
-puts '+------------+-----------------+-------------+'
+puts '+------------+-----+-----------+-------------+------------+'
+puts '| Date       | Duration (days) | Price (kr.) | Seats Left |'
+puts '+------------+-----------------+-------------+------------+'
 doc.css('.tcne-lms-hit').take(5).each do |hit|
   price = hit.at_css('.webui-price-formatter').text.scan(/\d/).join.chars.reverse.each_slice(3).map(&:join).join('.').reverse.rjust(5) << ',00'
   date  = Date.strptime(CGI.parse(URI.parse(hit.at_css('.tcne-lms-hit__content a')[:href]).query)['QueryDepDate'].first, '%d%m%Y').strftime('%Y-%m-%d')
+  seats = ((t = hit.at_css('.webui-tags__tag--few-seats')) && t.text.scan(/\d/).join).to_s.rjust(3)
 
-  puts "| #{date} |               8 |    #{price} |"
+  puts "| #{date} |               8 |    #{price} |        #{seats} |"
 end
-puts '+------------+-----------------+-------------+'
+puts '+------------+-----------------+-------------+------------+'
